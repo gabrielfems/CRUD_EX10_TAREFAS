d6e188be4388005e508f78888d1349e8d22ac12f
devgabs1 <gabriel.fe132@gmail.com>
2026-03-23 20:41:20 -0300

    Criando classe Repository

---
+++ src/main/java/com/atv/ex10/Repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package com.atv.ex10.Repository;
+
+import com.atv.ex10.Model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
